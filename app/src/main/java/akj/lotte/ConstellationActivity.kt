f31bbfc8b0cbc72d93c64199a26be833c4a5c7e6
jhh86 <gnsgml86!Q>
2020-05-24 23:08:06 +0900

    20200524_chapter5.7

--- app/src/main/java/akj/lotte/ConstellationActivity.kt
+++ app/src/main/java/akj/lotte/ConstellationActivity.kt
@@ -2,11 +2,13 @@ package akj.lotte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ConstellationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+        Toast.makeText(applicationContext, "ConstellationActivity 입니다.", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/akj/lotte/NameActivity.kt
+++ app/src/main/java/akj/lotte/NameActivity.kt
@@ -2,11 +2,13 @@ package akj.lotte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class NameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+        Toast.makeText(applicationContext, "NameActivity 입니다.", Toast.LENGTH_SHORT).show()
     }
 }
