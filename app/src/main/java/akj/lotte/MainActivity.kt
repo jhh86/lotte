decfaab5b5cb7e2c657bc4f86e9e7e5254e38915
jhh86 <gnsgml86!Q>
2020-06-14 23:35:03 +0900

    20200614_5.14_end

--- app/src/main/java/akj/lotte/MainActivity.kt
+++ app/src/main/java/akj/lotte/MainActivity.kt
@@ -1,14 +1,30 @@
 package akj.lotte
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(applicationContext, "MainActivity 입니다.", Toast.LENGTH_SHORT).show()
+
+        randomCard.setOnClickListener {
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(LottoNumberMaker.getShuffleLottoNumbers()))
+      startActivity(intent)
+        }
+
+        constellationCard.setOnClickListener {
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+
+        nameCard.setOnClickListener {
+            startActivity(Intent(this, NameActivity::class.java))
+        }
     }
 }
--- app/src/main/java/akj/lotte/NameActivity.kt
+++ app/src/main/java/akj/lotte/NameActivity.kt
@@ -1,14 +1,29 @@
 package akj.lotte
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
-        Toast.makeText(applicationContext, "NameActivity 입니다.", Toast.LENGTH_SHORT).show()
+        goButton.setOnClickListener {
+            if (TextUtils.isEmpty(editText.text.toString())) {
+                Toast.makeText(applicationContext, "이름을 입력하세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(LottoNumberMaker.getLottoNumbersFromHash(editText.text.toString())))
+            intent.putExtra("name", editText.text.toString())
+            startActivity(intent)
+        }
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
